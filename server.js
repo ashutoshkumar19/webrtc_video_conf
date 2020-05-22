19990a09c0e851ce893c66d80efae1d0a7506d85
Ashutosh Kumar <kumar.ashutosh920@gmail.com>
2020-05-23 04:59:15 +0530

    style updated

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
-const server = require('http').createServer(app);
+const http = require('http');
+const server = http.createServer(app);
+
 const io = require('socket.io')(server);
 
 users = [];
