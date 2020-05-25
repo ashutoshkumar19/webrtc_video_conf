c6acd1438a1e99ae2e412f236003145d6ee051ce
Ashutosh kumar <kumar.ashutosh920@gmail.com>
2020-05-26 03:50:25 +0530

    new turn server added

--- client/src/components/MainComponent.js
+++ client/src/components/MainComponent.js
@@ -71,17 +71,17 @@ const MainComponent = ({ socket }) => {
 
           var configuration = {
             iceServers: [
+              // {
+              //   urls: [
+              //     'stun:stun.l.google.com:19302',
+              //     'stun:stun1.l.google.com:19302',
+              //     'stun:stun2.l.google.com:19302',
+              //     'stun:stun3.l.google.com:19302',
+              //     'stun:stun4.l.google.com:19302',
+              //   ],
+              // },
               {
-                urls: [
-                  'stun:stun.l.google.com:19302',
-                  'stun:stun1.l.google.com:19302',
-                  'stun:stun2.l.google.com:19302',
-                  'stun:stun3.l.google.com:19302',
-                  'stun:stun4.l.google.com:19302',
-                ],
-              },
-              {
-                url: 'turn:numb.viagenie.ca',
+                urls: 'turn:numb.viagenie.ca',
                 credential: 'muazkh',
                 username: 'webrtc@live.com',
               },
